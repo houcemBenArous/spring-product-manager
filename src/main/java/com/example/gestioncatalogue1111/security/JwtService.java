01e5cfd679a73ab49df252849213a71ae7022a0c
houcemBenArous <houcem.benarous2001@gmail.com>
2025-11-11 12:40:36 +0100

    create jwtService

--- src/main/java/com/example/gestioncatalogue1111/security/JwtService.java
+++ src/main/java/com/example/gestioncatalogue1111/security/JwtService.java
@@ -1,8 +1,72 @@
 package com.example.gestioncatalogue1111.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JwtService {
 
-    @Value()
+    @Value("${spring.app.secretkey}")
+    private String secretKey;
+    @Value("${spring.app.expiration}")
+    private Long expiration;
+
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", userDetails.getAuthorities().stream().map(authority -> authority.getAuthority()).toList());
+        return Jwts.builder()
+
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getKey())
+                .compact();
+    }
+
+
+    private Claims getClaimsFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
+    public String getUsernameFromToken(String token) {
+        return getClaimsFromToken(token).getSubject();
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimsFromToken(token).getExpiration();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername())
+                && getExpirationDateFromToken(token).after(new Date()));
+    }
+
+
+    private SecretKey getKey() {
+        byte[] x = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(x);
+    }
+
+
+
+
+
+
+
 }
